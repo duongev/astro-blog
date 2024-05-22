246afb3fa7dd69a95bbc1d13e11c65fe9ac17f14
RobinFood <duongbv@bfast.dev>
2024-05-22 17:40:05 +0700

    add firebase login

---
+++ src/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxmG2-qNHhXdzSDwxyS0txih1hkeNS02Q",
+    authDomain: "blog-database-b66a1.firebaseapp.com",
+    projectId: "blog-database-b66a1",
+    storageBucket: "blog-database-b66a1.appspot.com",
+    messagingSenderId: "4253562809",
+    appId: "1:4253562809:web:dac716f8aa0f454256e868"
+};
+
+export const app = initializeApp(firebaseConfig);
